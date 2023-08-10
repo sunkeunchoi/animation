224efab5a33e6ad8630ecfffd35e937b0b012313
sunkeunchoi <sunkeun.choi@gmail.com>
2023-08-10 21:50:31 +0900

    2.0-MenuScreen

---
+++ lib/screens/menu_screen.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class MenuScreen extends StatelessWidget {
+  const MenuScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+      title: const Text("Flutter Animations"),
+    ));
+  }
+}
