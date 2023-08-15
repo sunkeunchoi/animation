08be1d03d77c6e0abe7c1c43056021511a0dcc57
sunkeunchoi <sunkeun.choi@gmail.com>
2023-08-15 11:54:46 +0900

    add appbar

--- lib/screens/menu_screen.dart
+++ lib/screens/menu_screen.dart
@@ -1,5 +1,6 @@
-import 'package:animation_class/screens/implicit_animations_screen.dart';
 import 'package:flutter/material.dart';
+
+import 'implicit_animations_screen.dart';
 
 class MenuScreen extends StatelessWidget {
   const MenuScreen({super.key});
@@ -12,20 +13,36 @@ class MenuScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text("Flutter Animations"),
-      ),
-      body: Center(
-        child: Column(
-          children: [
-            ElevatedButton(
-              onPressed: () {
-                _goto(context, const ImplicitAnimationsScreen());
-              },
-              child: const Text("Implicit Animations"),
-            )
+    return SafeArea(
+      child: Scaffold(
+        appBar: AppBar(
+          centerTitle: false,
+          titleSpacing: 0,
+          leading: const Padding(
+            padding: EdgeInsets.only(left: 12),
+            child: CircleAvatar(
+              foregroundImage: NetworkImage("https://avatars.githubusercontent.com/u/639005?v=4"),
+            ),
+          ),
+          actions: [
+            IconButton(
+              iconSize: 28,
+              onPressed: () {},
+              icon: const Icon(Icons.add_rounded),
+            ),
           ],
+        ),
+        body: Center(
+          child: Column(
+            children: [
+              ElevatedButton(
+                onPressed: () {
+                  _goto(context, const ImplicitAnimationsScreen());
+                },
+                child: const Text("Implicit Animations"),
+              )
+            ],
+          ),
         ),
       ),
     );
