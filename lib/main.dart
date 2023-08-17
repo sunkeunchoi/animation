0c01dfa825c8886fba45d02931d3a89f900bf5ee
sunkeunchoi <sunkeun.choi@gmail.com>
2023-08-17 22:08:21 +0900

    move challenge theme color

--- lib/main.dart
+++ lib/main.dart
@@ -22,37 +22,3 @@ class MyApp extends StatelessWidget {
     );
   }
 }
-
-const ColorScheme themeColors = ColorScheme(
-  brightness: Brightness.dark,
-  primary: Color(0xffeae1f8),
-  onPrimary: Color(0xff141414),
-  primaryContainer: Color(0xfffdf771),
-  onPrimaryContainer: Color(0xff000000),
-  secondary: Color(0xffece0ff),
-  onSecondary: Color(0xff141414),
-  secondaryContainer: Color(0xff956dc8),
-  onSecondaryContainer: Color(0xff000000),
-  tertiary: Color(0xffffdbe6),
-  onTertiary: Color(0xff141414),
-  tertiaryContainer: Color(0xffc6ed67),
-  onTertiaryContainer: Color(0xff000000),
-  error: Color(0xffffb4ab),
-  onError: Color(0xff690005),
-  errorContainer: Color(0xff93000a),
-  onErrorContainer: Color(0xffffb4ab),
-  background: Color(0xff1f1f1f),
-  onBackground: Color(0xfff1f1f1),
-  surface: Color(0xff1f1f1f),
-  onSurface: Color(0xfff1f1f1),
-  surfaceVariant: Color(0xff444346),
-  onSurfaceVariant: Color(0xff000000),
-  outline: Color(0xffa4337d),
-  outlineVariant: Color(0xff2e2e2e),
-  shadow: Color(0xff000000),
-  scrim: Color(0xff000000),
-  inverseSurface: Color(0xfffdfdfe),
-  onInverseSurface: Color(0xff131313),
-  inversePrimary: Color(0xff736f76),
-  surfaceTint: Color(0xffeae1f8),
-);
