0ac8ffb96848d3912bc1347c00ccb51a22e026c1
sunkeunchoi <sunkeun.choi@gmail.com>
2023-08-19 18:03:15 +0900

    finish detail screen

--- lib/screens/movie_screen.dart
+++ lib/screens/movie_screen.dart
@@ -18,7 +18,21 @@ class MovieScreen extends StatelessWidget {
           var data = snapshot.data!;
           return Scaffold(
             appBar: AppBar(
-              title: Text(data.title.toString()),
+              leading: IconButton(
+                onPressed: () {
+                  Navigator.pop(context);
+                },
+                icon: const Icon(
+                  Icons.arrow_back,
+                  color: Colors.white,
+                  shadows: [
+                    Shadow(
+                      color: Colors.black,
+                      blurRadius: 3,
+                    ),
+                  ],
+                ),
+              ),
               backgroundColor: Colors.transparent,
               elevation: 0,
             ),
@@ -28,23 +42,66 @@ class MovieScreen extends StatelessWidget {
                 Hero(
                   tag: tag,
                   child: Image.network(
-                    'https://image.tmdb.org/t/p/w500/${data.backdropPath}',
+                    'https://image.tmdb.org/t/p/w500/${data.posterPath ?? data.backdropPath}',
                     fit: BoxFit.cover,
-                    height: double.infinity,
-                    width: double.infinity,
-                    alignment: Alignment.center,
+                    height: MediaQuery.of(context).size.height,
+                    width: MediaQuery.of(context).size.width,
+                    // alignment: Alignment.center,
                   ),
                 ),
-                Column(
-                  children: [
-                    const Divider(color: Colors.transparent),
-                    Text(data.overview.toString()),
-                    const Divider(color: Colors.transparent),
-                    Text(data.imdbId.toString()),
-                    const Divider(color: Colors.transparent),
-                    Text(data.originalLanguage.toString()),
-                    const Divider(color: Colors.transparent),
-                  ],
+                DefaultTextStyle(
+                  style: const TextStyle(
+                    color: Colors.white,
+                    shadows: [
+                      Shadow(
+                        color: Colors.black,
+                        blurRadius: 1,
+                      ),
+                    ],
+                  ),
+                  child: Positioned(
+                    top: MediaQuery.of(context).size.height * 0.45,
+                    child: SizedBox(
+                      width: MediaQuery.of(context).size.width,
+                      child: Padding(
+                        padding: const EdgeInsets.symmetric(
+                          horizontal: 20,
+                        ),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              data.title.toString(),
+                              style: const TextStyle(
+                                fontSize: 26,
+                                fontWeight: FontWeight.w900,
+                              ),
+                            ),
+                            const Divider(color: Colors.transparent),
+                            // Text(data.getFavoriteRate().toString()),
+                            Stars(rate: data.getFavoriteRate()),
+                            const Divider(color: Colors.transparent),
+                            Text(data.getRunTimeAndGenres()),
+                            const Divider(color: Colors.transparent),
+                            const Text(
+                              "Storyline",
+                              style: TextStyle(
+                                fontSize: 20,
+                                fontWeight: FontWeight.w900,
+                              ),
+                            ),
+                            const Divider(color: Colors.transparent),
+                            Text(
+                              data.overview.toString(),
+                              style: const TextStyle(
+                                fontSize: 16,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                  ),
                 ),
               ],
             ),
@@ -52,6 +109,32 @@ class MovieScreen extends StatelessWidget {
         }
         return const CircularProgressIndicator();
       },
+    );
+  }
+}
+
+class Stars extends StatelessWidget {
+  const Stars({
+    super.key,
+    required this.rate,
+  });
+  final num rate;
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        for (var i = 0; i < 5; i++)
+          Icon(
+            Icons.star,
+            color: i < rate ? Colors.yellowAccent.shade700 : Colors.white,
+            shadows: const [
+              Shadow(
+                color: Colors.black,
+                blurRadius: 1,
+              ),
+            ],
+          ),
+      ],
     );
   }
 }
