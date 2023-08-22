1fdd1ba200da776eecaf2d7cc46668689b2142d6
sunkeunchoi <sunkeun.choi@gmail.com>
2023-08-22 19:29:46 +0900

    input decoration theme

--- lib/main.dart
+++ lib/main.dart
@@ -19,6 +19,35 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         colorScheme: onBoardingColors,
         useMaterial3: true,
+        inputDecorationTheme: InputDecorationTheme(
+          helperStyle: TextStyle(
+            fontSize: 16,
+            color: Colors.grey.shade600,
+            fontWeight: FontWeight.w400,
+          ),
+          helperMaxLines: 3,
+          suffixIconColor: Colors.green,
+          focusedBorder: const UnderlineInputBorder(
+            borderSide: BorderSide(
+              color: Colors.grey,
+            ),
+          ),
+          enabledBorder: const UnderlineInputBorder(
+            borderSide: BorderSide(
+              color: Colors.grey,
+            ),
+          ),
+          border: const UnderlineInputBorder(
+            borderSide: BorderSide(
+              color: Colors.grey,
+            ),
+          ),
+          labelStyle: const TextStyle(
+            color: Colors.grey,
+            fontSize: 24,
+            fontWeight: FontWeight.w500,
+          ),
+        ),
       ),
       home: const MenuScreen(),
     );
