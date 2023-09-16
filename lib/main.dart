8faa9e930c24521ce81c1892ffbb06d4279fc3d0
sunkeunchoi <sunkeun.choi@gmail.com>
2023-09-16 11:11:18 +0900

    add missing firebase_core

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:animation_class/firebase_options.dart';
 import 'package:animation_class/screens/nomad_coders/settings/settings_view_model.dart';
 import 'package:animation_class/screens/nomad_coders/theme.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:shared_preferences/shared_preferences.dart';
